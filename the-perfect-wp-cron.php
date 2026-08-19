e467bf86a31232a1c0b76df4ca79b7f86297d7c3
David Stone <david@nnucomputerwhiz.com>
2026-08-19 09:29:00 -0600

    fix: keep WordPress compatibility in readme only

--- the-perfect-wp-cron.php
+++ the-perfect-wp-cron.php
@@ -8,7 +8,6 @@
  * Author URI: https://ultimatemultisite.com
  * License: GPL-2.0-or-later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- * Tested up to: 7.1
  * Requires PHP: 8.1
  * Network: true
  */
